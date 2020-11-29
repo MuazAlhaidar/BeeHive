7bf95ad04005a9d7e45d1e33022cbb1fdcdceb0c
zaki <ahmedzakariya355@gmail.com>
2020-11-28 19:47:48 -0500

    I forgot this wasn't a javascript project
    Anyways, I got this workign in default tsx format. Use it as a frame of
    reference.
    
    DON"T MAKE IT TO A TS: import as the path of the file, don't import the
    extension. It won't work. I know from doing it bad:  the  ∑ of the
    confusion I was in

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./Images/Members App Logo - White Large.png";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import EventPage from "./pages/Event"
 
 function App() {
   return (
@@ -23,8 +24,7 @@ function App() {
 				      </p>
 			      </Route>
 			      <Route path="/Events">
-				      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.doomworld.com%2Fmonthly_2019_05%2F4ezIzWF.jpg.f74e8b271cfff7beb39ae87c1a168e7f.jpg&f=1&nofb=1"  className="App-logo" alt="logo" />
-				      <h1> Rolling downtown I need some friends now.</h1>
+				      <EventPage />
 			      </Route>
 		      </Switch>
 	      </Router>
---
+++ client/src/pages/Event.tsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+
+// export default function EventPage(){
+// export default const EventPage: React.FC = () => {
+export default function EventPage(){
+    const [name, setName] = React.useState("");
+    const [Description, setDescription] = React.useState("");
+    const [Address, setAddress] = React.useState("");
+    const [Date, setDate] = React.useState("");
+    const [Time, setTime] = React.useState("");
+    const [AuthorID, setAuthorID] = React.useState("");
+
+    const handleSubmit = (evt:any) => {
+	evt.preventDefault();
+	console.log( 
+	    {
+	    name:{name}.name,
+	    Description:{Description}.Description,
+	    Address:{Address}.Address,
+	    Date:{Date}.Date,
+	    Time:{Time}.Time,
+	    AuthorID:{AuthorID}.AuthorID,
+	    }
+	)
+	// TODO create a function to pass the data above to a event class
+	// To be appended to a table
+    }
+
+    return (
+	    <div>
+	    <h1> Send 'me The Event</h1>
+		    <form onSubmit={handleSubmit} >
+		    <label> Event Name </label><br/>
+		    <input type="text" id="Name" value={name} onChange={e => setName(e.target.value)} /><br/>
+
+		    <label> Description </label><br/>
+		    <input type="text" id="Description" value={Description} onChange={e => setDescription(e.target.value)} /><br/>
+
+		    <label> Address </label><br/>
+		    <input type="text" id="Address" value={Address} onChange={e => setAddress(e.target.value)} /><br/>
+
+		    <label> Date </label><br/>
+		    <input type="text" id="Date" value={Date} onChange={e => setDate(e.target.value)} /><br/>
+
+		    <label> Time </label><br/>
+		    <input type="text" id="Time" value={Time} onChange={e => setTime(e.target.value)} /><br/>
+
+		    <label> AuthorID </label><br/>
+		    <input type="text" id="AuthorID" value={AuthorID} onChange={e => setAuthorID(e.target.value)} /><br/>
+
+		    <input type="submit" value="Submit" />
+		    </form> 
+
+		    </div>
+    )
+}
