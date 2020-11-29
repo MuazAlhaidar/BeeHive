5bf697c445df8dde16eecbb501348e201a27b8ae
MuazAlhaidar <omunam10@gmail.com>
2020-11-28 22:19:33 -0500

    Cleared out the default stuff and created a Logo component that has a Logo and Title
    Yes, the picture size in Events is intentional
    Also i added .vscode to the .gitignore

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,45 @@
 import React from "react";
-import logo from "./Images/Members App Logo - White Large.png";
+import logo from "./Components/LogoAndTitle";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import LogIn from "./Pages/LogIn";
+import LogoAndTitle from "./Components/LogoAndTitle";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-	      <Router>
-		      <nav>
-			      <ul>
-				      <li> <Link to="/"> Welcome to Hell </Link> </li>
-				      <li> <Link to="/Events"> Events </Link> </li>
-			      </ul>
-		      </nav>
-		      <Switch>
-			      <Route exact path="/">
-				      <img src={logo} className="App-logo" alt="logo" />
-				      <h1> Hands off my property </h1>
-				      <p>
-					      Welcome to <code>BeeHive</code>!
-				      </p>
-			      </Route>
-			      <Route path="/Events">
-				      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.doomworld.com%2Fmonthly_2019_05%2F4ezIzWF.jpg.f74e8b271cfff7beb39ae87c1a168e7f.jpg&f=1&nofb=1"  className="App-logo" alt="logo" />
-				      <h1> Rolling downtown I need some friends now.</h1>
-			      </Route>
-		      </Switch>
-	      </Router>
+        <Router>
+          <div className="App-navbar">
+            <Link to="/"> Welcome to Hell </Link>
+            <Link to="/Events"> Events </Link>
+            <Link to="/LogIn"> LogIn </Link>
+          </div>
+          <Switch>
+            <Route exact path="/">
+              <div className="App-LogoAndTitle">
+                <LogoAndTitle />
+              </div>
+              <h1> Hands off my property </h1>
+              <p>
+                Welcome to <code>BeeHive</code>!
+              </p>
+            </Route>
+            <Route path="/Events">
+              <img
+                src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.doomworld.com%2Fmonthly_2019_05%2F4ezIzWF.jpg.f74e8b271cfff7beb39ae87c1a168e7f.jpg&f=1&nofb=1"
+                className="App-logo"
+                alt="logo"
+              />
+              <h1> Rolling downtown I need some friends now.</h1>
+            </Route>
+            <Route path="/LogIn">
+              {" "}
+              <LogIn />
+            </Route>
+          </Switch>
+        </Router>
       </header>
     </div>
   );
---
+++ client/src/Pages/LogIn.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import LogoAndTitle from "../Components/LogoAndTitle";
+
+import "../CSS/LogIn.css";
+
+function LogIn() {
+  return (
+    <div className="Login">
+      <header className="Login-Header">
+        <LogoAndTitle />
+      </header>
+    </div>
+  );
+}
+
+export default LogIn;
