96a6ad5239d42898ed6b51d015f15ee469327670
klongwor <klongwor@umich.edu>
2020-12-14 21:14:51 -0500

    changed contact info

--- client/src/Components/ContactUsInfo.tsx
+++ client/src/Components/ContactUsInfo.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function ContactUsInfo() {
   return (
     <div>
-      <div>Group Name: Two and Half Yemenis</div>
+      <div>Group Name: Beehive</div>
       <div>Group Number: 3</div>
       <div>Contact Email: thisisanemail@gmail.com</div>
     </div>
