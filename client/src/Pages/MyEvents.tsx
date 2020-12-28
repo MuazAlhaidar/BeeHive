8b46b39f0f2661f4563106dc8149a2519e01c8a3
MuazAlhaidar <omunam10@gmail.com>
2020-12-27 22:04:35 -0500

    Created designated zones for each of the features

--- client/src/Pages/MyEvents.tsx
+++ client/src/Pages/MyEvents.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "../CSS/Events/MyEvents.css";
 
 function MyEvents() {
-  return <div></div>;
+  return (
+    <div className="MyEvents">
+      <div className="MyEvents-EventList"></div>
+      <div className="MyEvents-EventManager"></div>
+    </div>
+  );
 }
 
 export default MyEvents;
--- client/src/Pages/MyGroups.tsx
+++ client/src/Pages/MyGroups.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "../CSS/Groups/MyGroups.css";
 
 function MyGroups() {
-  return <div></div>;
+  return (
+    <div className="MyGroups">
+      <div className="MyGroups-GroupList"></div>
+      <div className="MyGroups-GroupManager"></div>
+      <div className="MyGroups-MemberList"></div>
+    </div>
+  );
 }
 
 export default MyGroups;
