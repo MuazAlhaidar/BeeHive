eae3842300141445058669c651301305ec02ced8
zaki <ahmedzakariya355@gmail.com>
2020-12-29 17:18:02 -0500

    WHAT IS THIS
    OMG WHY REUSIGN A MODEL BREAKS THE WHOEL COMPUTER

--- server/index.ts
+++ server/index.ts
@@ -13,73 +13,77 @@ const User = require('./model/User.ts');
 const Test = require('./model/Test.ts');
 
 // Dtabase initalize
-async function initialize() {
-	// create db if it doesn't already exist
-	const db = require('./config/config.json')["development"];
+// async function initialize() {
+// create db if it doesn't already exist
+const db = require('./config/config.json')["development"];
 
-	const {Sequelize} = require('sequelize');
-	const mariadb = require('mariadb')
-	const pool = mariadb.createPool({
-		host: db.host,
-		user: db.username,
-		password: db.password,
-		connectionLimit: 5
-	});
-	pool.getConnection()
-	.then(conn => {
-		conn.query(`CREATE database if not exists  ${db.database} ;`)
-		const sequelize = new Sequelize(db.database, db.username, db.password,{
-			dialect: 'mariadb',
-			logging: console.log
-		})
-		// Creating tables based on models
+const {Sequelize} = require('sequelize');
+// const mariadb = require('mariadb')
+// const pool = mariadb.createPool({
+// 	host: db.host,
+// 	user: db.username,
+// 	password: db.password,
+// 	connectionLimit: 5
+// });
+// pool.getConnection()
+// 	.Then(conn => {
+// 	    conn.query(`CREATE database if not exists  ${db.database} ;`)
+const sequelize = new Sequelize("studentdev", "rwx", "I am Sofa King.", {
+    logging:false,
+    dialect:"mariadb"
+})
 
-		/* In the event the tables/database is not setup/ or change size, this overrides that */
-		/* Note: this means that if the models get change, then the program will delete the table*/
-		/* And replace it with the new model */
-		/* TODO once we get this in production, we have to disable this behavior. Otherwise*/
-		/* We will be made an example of for crap developers */
-		/* So be wary of this section in production: please please please please please*/
-		/* remember this, and don't gloss over it */
+// const sequelize = new Sequelize(db.database, db.username, db.password,{
+// 	logging: false,
+// 	dialect: 'mariadb'
+// })
+// Creating tables based on models
 
-		// sequelize.sync()
-                // If DEV=true, it will erase everything. 
-		if( process.env.DEV == "true"){
-			// sequelize.sync()
-			sequelize.sync({force:true})
-			.then(() => {
-				Test.sync({force:true});
-				User.sync({force:true});
-			});
-		}
-		else{
-			sequelize.sync({force:false})
-			.then(() => {
-				Test.sync({force:false});
-				User.sync({force:false});
-			});
-		}
-	})
-	.catch(err =>{
-		console.log(err);
-	});
+/* In the event the tables/database is not setup/ or change size, this overrides that */
+/* Note: this means that if the models get change, then the program will delete the table*/
+/* And replace it with the new model */
+/* TODO once we get this in production, we have to disable this behavior. Otherwise*/
+/* We will be made an example of for crap developers */
+/* So be wary of this section in production: please please please please please*/
+/* remember this, and don't gloss over it */
+
+// sequelize.sync()
+// If DEV=true, it will erase everything. 
+//     if( process.env.DEV == "true"){
+// 	// sequelize.sync()
+// 	sequelize.sync({force:true})
+// 	    .then(() => {
+// 		Test.sync({force:true});
+// 		User.sync({force:true});
+// 	    });
+//     }
+//     else{
+// 	sequelize.sync({force:false})
+// 	    .then(() => {
+// 		Test.sync({force:false});
+// 		User.sync({force:false});
+// 	    });
+//     }
+// })
+// .catch(err =>{
+//     console.log(err);
+// });
 
 
 
 
-	// For ecah table in database
-	// If table is not same as model, override table
-}
+// For ecah table in database
+// If table is not same as model, override table
+// }
 
-initialize();
+// initialize();
 // // Routing
 // Routes setup
-// const users = require('./routes/users.ts'); app.use('/api/users', users);
-const test = require('./routes/test.ts'); app.use('/api/test', test);
-const user = require('./routes/users.ts'); app.use('/api/user', user);
+const users = require('./routes/users.ts'); app.use('/api/users', users);
+// const test = require('./routes/test.ts'); app.use('/api/test', test);
+// app.use('/api/user', users);
 
 const port = process.env.PORT || 4200;
 
-console.log("darkness");
 app.listen(port, () => console.log(`Server started at ${port}`));
 export {}
