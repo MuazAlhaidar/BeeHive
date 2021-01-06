09190c5fd11993d257441533dfa1d9297e43b31e
omunam <omunam@pop-os.localdomain>
2021-01-06 15:16:23 -0500

    Setting up for group management

---
+++ client/src/Components/Members/MemberList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MemberList() {
+  return <div></div>;
+}
+
+export default MemberList;
--- client/src/Pages/MyGroups.tsx
+++ client/src/Pages/MyGroups.tsx
@@ -1,12 +1,69 @@
 import React from "react";
+import GroupList from "../Components/Groups/GroupsList";
+import GroupForm from "../Components/Groups/GroupsForm";
+import MemberList from "../Components/Members/MemberList";
 import "../CSS/Groups/MyGroups.css";
 
+interface MemberInfo {
+  name: string;
+}
+
+interface GroupInfo {
+  name: string;
+  contactInfo: string;
+  members: Array<MemberInfo>;
+}
+
 function MyGroups() {
+  const fakeGroup = Array<GroupInfo>(
+    {
+      name: "HR Group",
+      contactInfo: "313-995-7488",
+      members: [{ name: "john" }, { name: "Thomas" }],
+    },
+    {
+      name: "PR Group",
+      contactInfo: "313-555-6598",
+      members: [{ name: "john" }],
+    }
+  );
+
+  const [groups, setGroups] = React.useState(fakeGroup);
+  const [groupIndex, setGroupIndex] = React.useState(0);
+
+  const selectGroup = (i: number) => {
+    let index = i === undefined ? 0 : i;
+    setGroupIndex(index);
+  };
+
+  const addGroup = (
+    name: string,
+    contactInfo: string,
+    members: Array<MemberInfo>
+  ) => {
+    const g = groups.slice();
+    g.push({ name, contactInfo, members });
+    setGroups(g);
+  };
+
+  const removeGroup = (i: number) => {
+    const g = groups.slice();
+    g.splice(i, 1);
+    setGroups(g);
+    setGroupIndex(groups.length);
+  };
+
   return (
     <div className="MyGroups">
-      <div className="MyGroups-GroupList"></div>
-      <div className="MyGroups-GroupManager"></div>
-      <div className="MyGroups-MemberList"></div>
+      <div className="MyGroups-GroupList">
+        <GroupList />
+      </div>
+      <div className="MyGroups-GroupForm">
+        <GroupForm />
+      </div>
+      <div className="MyGroups-MemberList">
+        <MemberList />
+      </div>
     </div>
   );
 }
