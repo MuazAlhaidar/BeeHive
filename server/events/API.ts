dcbacc396d21e2dad614571c053d630a5343b96d
zaki <ahmedzakariya355@gmail.com>
2021-01-09 15:00:29 -0500

    WTF

--- server/events/API.ts
+++ server/events/API.ts
@@ -3,46 +3,50 @@ const axios = require("axios")
 let baseurl = "http://localhost:4200/api/events/";
 
 async function axiosGet(url:string, _data:any){
-    return axios.get(baseurl+url, {params: _data})
+        return axios.get(baseurl+url, {params: _data})
 }
 async function axiosPost(url:string, _data:any){
-    return axios.post(baseurl+url,   _data , {headers:{'Content-Type':'application/json'}})
+        return axios.post(baseurl+url,   _data , {headers:{'Content-Type':'application/json'}})
 }
 
 
-async function newEvent(name:string, desc:string, address:string, time:Date, manager:number):Promise<Boolean>{
-    return axiosPost("new", {Name:name,  Description:desc, Address:address, Time:time, Manager:manager})
-	.then(ret => true)
-	.catch(err =>  false)
+async function newEvent(name:string, desc:string, address:string, time:Date, manager:number){
+        return axiosPost("new", {Name:name,  Description:desc, Address:address, Time:time, Manager:manager})
+        .then(ret => {  console.log("THIS IS WHATS RETURN", ret.data); return ret.data })
+        .catch(err => {console.log("Welp fuck"); return undefined})
 }
 
 async function getEvent(_id:number){
-    // return axios.get(baseurl+"get", {params:{
-    // 	id:_id
-    // }})
-    
-    return axiosGet("get", {id:_id})
-	.then(res => res)
-	.catch(err =>  { {}} )
-    
+        return axiosGet("get", {id:_id})
+        .then(res => {
+                if(res.data===''){
+                        return undefined
+                }
+                else{
+                        return res.data
+                }
+        })
+        .catch(err =>  { {}} )
+
 }
 
-async function update(_id:number, _name:undefined|string, _desc:undefined|string, _address:undefined|string, _time:undefined|Date, _manager:undefined|number){
-    return axiosPost("update2", {id:_id, Name:_name,  Description:_desc, Address:_address, Time:_time, Manager:_manager})
-	.then(res => res)
-	.catch(err => { {}})
+async function update(_id:number, _name:undefined|string, _desc:undefined|string, _address:undefined|string, _time:undefined|Date){
+        // return axiosPost("update2", {id:_id, Name:_name,  Description:_desc, Address:_address, Time:_time, Manager:_manager})
+        return axiosPost("update", {id:_id, Name:_name,  Description:_desc, Address:_address, Time:_time})
+        .then(res => res.data)
+        .catch(err => { {}})
 }
 
 async function getAllEvents(){
-    return axiosGet("getall", undefined)
-	.then(res => res)
-	.catch(err => { undefined})
+        return axiosGet("getall", undefined)
+        .then(res => res.data)
+        .catch(err => { undefined})
 }
 
 async function Delete(_id:number){
-    return axiosPost("delete", {id:_id})
-	.then(res => {return true})
-	.catch(err => false)
+        return axiosPost("delete", {id:_id})
+        .then(res => {return true})
+        .catch(err => false)
 }
 
 export {axiosGet, axiosPost, newEvent, getEvent, update, getAllEvents, Delete}
--- server/events/Route.ts
+++ server/events/Route.ts
@@ -10,14 +10,14 @@ const {or, and, gt, lt} = Sequelize.Op;
  * @desc Get all the events
  */
 router.get("/getall", (req,res)=>{
-    Event.findAll()
-	.then(ret => {
+        Event.findAll()
+        .then(ret => {
                 const values = ret.map(x => x.dataValues);
                 res.status(200).send(values);
         })
-        // .catch(err => {console.log(err); res.sendStatus(404)})
-        .catch(err => {res.sendStatus(404)})
-    
+        .catch(err => {console.log(err); res.sendStatus(404)})
+        // .catch(err => {res.sendStatus(404)})
+
 })
 /* @route POST api/events/new
  * @desc Create a new event
@@ -30,23 +30,11 @@ router.post("/new", (req,res)=>{
                         return;
                 }
         }
-        Event.create({
-         Name: req.body.Name
-        ,Description: req.body.Description
-        ,Address: req.body.Address
-        ,Time: req.body.Time
-        })
+        Event.create({ Name: req.body.Name ,Description: req.body.Description ,Address: req.body.Address ,Time: req.body.Time })
         .then(ret =>   {
-                // User.findAll({where:{ username:req.body.Manager}})
-                EventMember.create({
-                        User: req.body.Manager
-                ,Event: ret.dataValues.id
-                ,Attended: false
-                ,RSVP: false
-                ,Manager:true
-                })
-                .then(() =>{res.sendStatus(200)})
-                .catch(err => {res.status(404).send(err)})
+                EventMember.create({ User: req.body.Manager ,Event: ret.dataValues.id ,Attended: false ,RSVP: false ,Manager:true })
+                .then(ret2 => { res.status(200).send({Event:ret.dataValues, Member:ret2.dataValues})})
+                .catch(err => {res.status(403).send(err)})
 
         })
         .catch(err => res.status(404).send(err))
@@ -62,53 +50,24 @@ router.get("/get", (req, res)=>{
         Event.findOne({
                 where:{id:req.query.id}
         })
-        .then(ret => res.status(200).send(ret))
-        .catch(err =>  res.status(404).send(err))
+        .then(ret => {
+                if(ret===null){
+                        res.status(200).send("");
+                }
+                else
+                        res.status(200).send(ret)
+        })
+        .catch(err =>  res.status(403).send(err))
 })
 
 
-/* @route Post api/events/update/$id
- * @desc Update a specific event
- */
-/*
-   router.post("/update", (req, res)=>{
-// console.log(req.body)
-Event.update({
-Name: req.body.Name
-,Description: req.body.Description
-        ,Address: req.body.Address
-,Time: req.body.Time
-    }, {where:{
-id:req.body.id
-}})
-.then(ret => {
-// TODO update this to incldue event member
-// EventMember.update({User: req.body.Manager},
-// 		       where:{
-
-// 		       })
-// .then(() => res.status(200).send({Name: req.body.Name ,Description: req.body.Desc ,Address: req.body.Address ,Time: req.body.Time}) )
-res.status(200).send() 
-// .then(() => res.status(200).send() )
-})
-.catch(err => res.status(404).send("Unable to update event") )
-
-})
-*/
-
-router.post("/update2", (req, res)=>{
+router.post("/update", (req, res)=>{
         // console.log(req.body)
         Event.findOne({where:{id:req.body.id}})
         .then(ret => {
-                EventMember.findOne({where:{Event:req.body.id}})
-                .then(ret2 => {
-                        // console.log("WHat is found in event and eventmember", ret, ret2)
-                        // res.sendStatus(404)
-                        Promise.all([Event.update({Name: req.body.Name ,Description: req.body.Description ,Address: req.body.Address ,Time: req.body.Time}, {where:{id:req.body.id}}), EventMember.update({User:req.body.Manager}, {where:{id:ret.dataValues.id}})])
+                Promise.all([Event.update({Name: req.body.Name ,Description: req.body.Description ,Address: req.body.Address ,Time: req.body.Time}, {where:{id:req.body.id}})])
 
-                        // // TODO Try to fix this hack
-                        res.status(200).send({Event:{ Name: req.body.Name ,Description: req.body.Description ,Address: req.body.Address ,Time: req.body.Time} , Manager:{User:req.body.Manager}})
-                })
+                res.status(200).send({Event:{ Name: req.body.Name ,Description: req.body.Description ,Address: req.body.Address ,Time: req.body.Time} })
         })
         .catch(err => res.status(404).send("Unable to update event") )
 
@@ -117,9 +76,9 @@ router.post("/update2", (req, res)=>{
 router.post("/delete", (req, res)=>{
         EventMember.destroy({ where: {Event: req.body.id}})
         .then( ret=>{
-                   Event.destroy({where:{id: req.body.id}})
-                   .then( () => {res.sendStatus(200); return})
-                   .catch( err => { res.status(404).send(err); return})
+                Event.destroy({where:{id: req.body.id}})
+                .then( () => {res.sendStatus(200); return})
+                .catch( err => { res.status(404).send(err); return})
         })
         .catch( err => { res.status(404).send(err); return})
 })
