d32865ce9296c55820cb82e243813d987580d9bf
zaki <ahmedzakariya355@gmail.com>
2021-02-12 16:02:17 -0500

    CRUD Groups
    Almost done with work for today :)

--- server/users/Model.ts
+++ server/users/Model.ts
@@ -16,14 +16,16 @@ const User = sequelize.define('user', {
     // NOTE:  This will only worked iwth MySQL/MariaDB, as explaiend in https://sequelize.org/master/manual/model-basics.html#data-types
     // If this cannot be allowed, make type for poins BIGINT(11)
 
-    ,points:{type: Sequelize.INTEGER.UNSIGNED, allowNull: false}
-    ,role_id:{type: Sequelize.INTEGER, allowNull: false}
+    ,points:{type: Sequelize.INTEGER.UNSIGNED, allowNull: false, defaultValue:0}
+    ,role_id:{type: Sequelize.INTEGER, allowNull: false,  defaultValue:0}
     
     // For reseting password only
     ,resetPassword_token:{type: Sequelize.STRING, allowNull: true}
 
     // NOTE: https://sequelize.org/master/manual/model-basics.html a weird mention to DataType.Date for mysql/sqlite, thing. I'm not sure, so just make note of this
     ,resetPassword_expiry:{type: Sequelize.DATE, allowNull: true}
+
+    ,isOwner:{type: Sequelize.BOOLEAN, allowNull: true, defaultValue:false}
 })
 
 
