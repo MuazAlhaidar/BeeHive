8b45d986620d7819bca2dd7cdd39dd85fd4c37d4
MuazAlhaidar <omunam10@gmail.com>
2021-03-15 11:11:54 -0400

    Added firstname and lastname to frontend

--- client/src/Components/SignInPanel.tsx
+++ client/src/Components/SignInPanel.tsx
@@ -1,44 +1,66 @@
 import React from "react";
-import "../CSS/LogInPanel.css";
+import "../CSS/SignUpPanel.css";
 
 function SignInPanel(props: { addUser: any }) {
+  const [firstname, setFirstname] = React.useState("");
+  const [lastname, setLastname] = React.useState("");
   const [username, setUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [email, setEmail] = React.useState("");
 
   return (
-    <div className="LoginPanel">
+    <div className="SignUpPanel">
       <form>
-        <p className="LoginPanel-InputTitle">Enter a New Username</p>
+        <div>
+          <p className="SignUpPanel-InputTitle">Enter your Firstname</p>
+          <input
+            className="SignUpPanel-Input"
+            type="text"
+            onChange={(e) => setFirstname(e.target.value)}
+            id="Firstname"
+            value={firstname}
+          />
+
+          <p className="SignUpPanel-InputTitle">Enter your Lastname</p>
+          <input
+            className="SignUpPanel-Input"
+            type="text"
+            onChange={(e) => setLastname(e.target.value)}
+            id="Lastname"
+            value={lastname}
+          />
+        </div>
+
+        <p className="SignUpPanel-InputTitle">Enter a new Username</p>
         <input
-          className="LoginPanel-Input"
+          className="SignUpPanel-Input"
           type="text"
           onChange={(e) => setUsername(e.target.value)}
           id="Username"
           value={username}
         />
 
-        <p className="LoginPanel-InputTitle">Enter your email</p>
+        <p className="SignUpPanel-InputTitle">Enter your Email</p>
         <input
-          className="LoginPanel-Input"
+          className="SignUpPanel-Input"
           type="text"
           onChange={(e) => setEmail(e.target.value)}
           id="Email"
           value={email}
         />
 
-        <p className="LoginPanel-InputTitle">Enter a New Password</p>
+        <p className="SignUpPanel-InputTitle">Enter a new Password</p>
         <input
-          className="LoginPanel-Input"
+          className="SignUpPanel-Input"
           type="password"
           id="Password"
           onChange={(e) => setPassword(e.target.value)}
           value={password}
         />
       </form>
-      <div className="LoginPanel-Buttons">
+      <div className="SignUpPanel-Buttons">
         <button
-          className="LoginPanel-Login"
+          className="SignUpPanel-SignUpButton"
           onClick={() => props.addUser(username, password, email)}
         >
           Sign Up
