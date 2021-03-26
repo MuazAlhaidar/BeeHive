f9bda9e596ede05028247eadec18648fe602e975
zaki <ahmedzakariya355@gmail.com>
2021-03-25 23:35:54 -0400

    Did firebase for user.ts

--- client/src/api/User.ts
+++ client/src/api/User.ts
@@ -1,93 +1,70 @@
-const axios = require("axios")
-// Change port to be dynamic,
-let baseurl = "http://localhost:4200/api/users/";
-
-async function axiosGet(url:string, _data:any){
-        // return axios.get(baseurl+url, { data: {_data}  })
-        // return axios.get(baseurl+url, { data: _data  }, {headers:{'Content-Type':'application/json'}} )
-		return axios.get(baseurl+url, _data,{
-			headers:{ 'Content-Type': 'application/json' }
-		})
-
-
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import {config} from "./config.js"
+firebase.initializeApp(config);
+const db = firebase.firestore()
+const auth = firebase.auth()
+interface Message{
+        data:any,
+        msg:string|number
 }
-async function axiosPost(url:string, _data:any){
-        return axios.post(baseurl+url,   _data , {headers:{'Content-Type':'application/json'}})
+function genMessage(_data:any,_msg:any){ return {msg:_msg, data:_data}}
+function testFunc(args, func){
+        func(args).then(res=>{console.log(res)}).catch(res=>{console.log(res)})
 }
-
-async function login(_username:string, _password:string){
-        // return axiosPost("login", { username:_username, password:_password} )
-        return axiosPost("login", { username:_username, password:_password} )
-        .then(res => [res.data.id, res.data.owner])
-        .catch(err =>[-1,-1])
-}
-
-
-async function new_user( user:any ){
-    return axiosPost("new",  {
-        username:user.username,
-        password:user.password,
-        email:user.email,
-        role_id:user.role_id,
-        firstname:user.firstname,
-        lastname:user.lastname,
-        points:user.points 
-
-    })
-        .then(ret => {return [0, ret.data.id]})
-        .catch((err:any)=>{
-                switch(err.response.status){
-                        case 401:
-                                return [1, undefined];
-                        case 402:
-                                return [2, undefined];
-                        case 403:
-                                return [3,undefined];
-                        default:
-                                return err.response.status
-                }
+async function login(email:string, password:string):Promise<Message>{
+        const cityRef = db.collection('Users').doc('moniera@umich.edu');
+        const doc = await cityRef.get();
+        return firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(async res=>{ 
+                return db
+                .collection('Users')
+                .doc(email)
+                .get()
+                .then(re2=> {
+                        return genMessage(re2.data(), "success")
+                })
         })
-
+        .catch(res=>genMessage(false, "failed to login") )
 }
-
-async function reset_password(_email:string, _test:boolean=false){
-        return axiosPost("reset_request", {email:_email, test:_test})
-        .then(ret =>  true )
-        .catch(ret => false )
-
+async function new_user(email,  password, fName, lName):Promise<Message>{
+        // async function new_user(email,  password, fName, lName){
+        return auth.createUserWithEmailAndPassword(email, password)
+        .then(res=>{ 
+                return db.collection("Users").doc(email).get()
+                .then((snapshot)=>{
+                        if(!snapshot.exists){
+                                let user={FirstName: fName, LastName: lName, email: email, userPoints: 0,}
+                                db.collection('Users').doc(email).set(user)
+                                return genMessage(user, "Success")
+                        }
+                        else{
+                                return genMessage(1, "User already exists")
+                        }
+                })
+                .catch(err=>genMessage(3, "Failed to login"))
+        })
+        .catch(err=>genMessage(2, "User already exists"))
 }
-
-async function reset_token(_token:string, _newpass:string){
-        return axiosPost("reset_token",{token:_token, password:_newpass} )
-        .then(res => true)
-        .catch(res => false)
-
+async function reset_password(_email:string):Promise<Message>{
+        return auth.sendPasswordResetEmail(_email)
+        .then(res=>genMessage(true, "Reset password email sent"))
+        .catch(res=>genMessage(false, "Failed to send password"))
 }
-async function reset_url(_token:string){
-        return axiosPost("reset_url",{token:_token} )
-        .then(res => true)
-        .catch(res => false)
-}
-async function points(_id:number, _points:number){
-        return axiosPost("points", {id:_id, points:_points})
-        .then(res=>true)
-        .then(res=>false)
-}
-
-async function getall(){
-        return axiosGet("getall", {})
-        .then(res=>res.data)
+async function getall():Promise<Message>{
+        return db.collection("Users").get()
+        .then(res=>res.docs.map(x=> x.data()))
         .catch(res=>res)
-
 }
-async function changeemail(_id:any, _newemail:any){
-    return axiosPost("changeemail", {id:_id, newemail:_newemail})
-        .then(res=>true)
-        .catch(res=>false)
+async function changeemail(email:any):Promise<Message>{
+        var user = firebase.auth().currentUser;
+        return user.updateEmail(email)
+        .then( () =>{
+                return genMessage(true, "Changed email")
+        })
+        .catch( (error) =>{
+                return genMessage(false, "Coudln't change email,"+ error)
+        });
 }
-
-
-export {axiosPost,axiosGet,  login,new_user,reset_password, reset_token, reset_url, points, getall, changeemail}
-
-// module.exports= { login,new_user,reset_password, reset_token}
-
+export {login, new_user, reset_password, getall, changeemail}
