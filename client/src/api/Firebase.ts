8703a6b67436e9e19693dced07e2791dd45509d3
zaki <ahmedzakariya355@gmail.com>
2021-04-09 15:56:24 -0400

    I think I made the basic API calls

--- client/src/api/Firebase.ts
+++ client/src/api/Firebase.ts
@@ -25,115 +25,88 @@ export async function query(collection, queryTerm=undefined, query=undefined){
 }
 export async function newDoc(collection, entry,queryTerm=undefined  ){
         const ref = Fire.default.firestore().collection(collection)
-        let result = query(collection, entry, queryTerm)
-        /*
-        if(queryTerm!==undefined){
-                let find = await ref.where(queryTerm, "==",entry[queryTerm]).get()
-                .then((documentSnapshot: any) => {
-                        // If the document already exists then fail
-                        // console.log(queryTerm, entry[queryTerm])
-                        if (documentSnapshot.size!=0) {
-                                return genMessage(
-                                        -1,
-                                        "This Event already exists. \n Please enter a new Name."
-                                );
-                        }
-                        else {
-                                return true; 
-                        }
-                });
-
-                if (find === true) {
-                        // If the event does not exist
-                        return ref
-                        .doc() // We use a title as the ID for an event
-                        .set(entry)
-                        .then((res: any) => genMessage(entry["queryTerm"], "Made a new "+collection))
-                        .catch((err: any) => genMessage(err, "Failed to make a "+collection));
-                } else return find;
-        }
-        else{
-                // If the event does not exist
+        function makeDoc(entry){
                 return ref
                 .doc() // We use a title as the ID for an event
                 .set(entry)
-                .then((res: any) => genMessage(entry["queryTerm"], "Made a new "+collection))
+                .then((res: any) => genMessage(true, "Made a new "+collection))
                 .catch((err: any) => genMessage(err, "Failed to make a "+collection));
         }
-        */
-       return result
-
-
+        if(queryTerm === undefined){
+                return makeDoc(entry)
+        }
+        else {
+                let result = await query(collection,  queryTerm, entry[queryTerm])
+                if(result.size >0){
+                        return genMessage(-1, collection + "  already exist for value " + queryTerm + " which is "  + entry[queryTerm])
+                }
+                else{
+                        return makeDoc(entry)
+                } 
+        }
 }
+
 
 export async function updateDoc(collection, entry, id, queryTerm=undefined){
         const ref = Fire.default.firestore().collection(collection)
-        if(queryTerm!==undefined){
-                let find = await ref.where(queryTerm, "==", entry[queryTerm]).get()
-                .then((documentSnapshot: any) => {
-                        // If the document already exists then fail
-                        // console.log(queryTerm, entry[queryTerm])
-                        if (documentSnapshot.size!=0) {
-                                return genMessage(
-                                        -1,
-                                        "This Event already exists. \n Please enter a new Name."
-                                );
-                        }
-                        else {
-                                return true; 
-                        }
-                });
-
-                if (find === true) {
-                        // If the event does not exist
-                        return ref
-                        .doc(id) 
-                        .update(entry)
-                        .then((res: any) => genMessage(entry["queryTerm"], "Update a new "+collection))
-                        .catch((err: any) => genMessage(err, "Failed to update a "+collection));
-                } else return find; 
-        }
-        else{
-                // If the event does not exist
+        function updateDoc(id,entry){
                 return ref
-                .doc(id) 
+                .doc(id) // We use a title as the ID for an event
                 .update(entry)
-                .then((res: any) => genMessage(entry["queryTerm"], "Update a new "+collection))
+                .then((res: any) => genMessage(true, "Updated "+collection))
                 .catch((err: any) => genMessage(err, "Failed to update a "+collection));
         }
+        if(queryTerm === undefined){
+                console.log(queryTerm, "Am i fucking undefined?")
+                return updateDoc(id, entry)
+        }
+        else {
+                let result = await query(collection,  queryTerm, entry[queryTerm])
+                if(result.size >0){ 
+                        // Because we could be updating a key vlaue that already exist, we have to chekc for thisk
+                        let found = (result.docs.map(x=>getid(x)))[0]
+                        if(found.id === id){
+                                return updateDoc(id,entry)
+                        }
+                        else{
+                                return genMessage(-1, collection + "  already exist for value " + queryTerm)
+                        }
+                }
+                else{
+                        return updateDoc(id,entry)
+                } 
+        }
 
 }
-export async function getDoc(collection, queryTerm=undefined, query=""){
+export async function getDoc(collection, queryTerm=undefined, cquery=""){
         const ref = Fire.default.firestore().collection(collection)
-        if(queryTerm===undefined){
-                ref.where(queryTerm, "==", query).get()
-                .then((documentSnapshot: any) => {
-                        return genMessage(
-                                documentSnapshot.docs.map((x:any)=>getid),
-                                        "Got all stuff")
-                })
+        function getALLdocs(res){
+                return genMessage(res.docs.map((x:any)=>getid(x)), "Got documents for " + collection + " that matches" + cquery)
         }
-        else{
-                return ref.get().then((res:any)=>{
-                        return res.docs.map((x:any)=> getid(x))
-                })
-
-        }
+        let result = await query(collection, queryTerm, cquery)
+        return getALLdocs(result)
 }
 
-export async function subquery(collection, subcollection, subterm:string, subqueryTerm=undefined, queryTerm=undefined, cquery=undefined){
+export async function getDocsSub(collection, subcollection, subterm:string, subqueryTerm=undefined, queryTerm=undefined, cquery=undefined){
         let main = await  query(collection, queryTerm, cquery)
-        let tmp= Promise.all(main.docs.map((doc:any)=> { 
-                let data = doc.data()
-                return Promise.all( data[subterm].map(async (single)=>{
-                        // console.log(subcollection, subqueryTerm, single)
-                        return query(subcollection, subqueryTerm, single)
+        let enteries = main.docs.map(x=>getid(x))
+        let tmp= Promise.all(enteries.map( async doc=>{
+                let subs= await Promise.all(doc[subterm].map(async entry=>{
+                        let found = await query(subcollection, subqueryTerm, entry)
+                        // Reason we want [0] is because the query form above
+                        // should only return one entyr:
+                        // ala we are getting the primary key for thid doc
+                        let tmp =  found.docs.map(x=>getid(x))[0]
+                        return  tmp
                 }))
+                doc["."+subterm] = subs
+                // return doc
+                return doc
         }))
+        return tmp
 }
 
 export async function update(collection, id, obj){
         const ref = Fire.default.firestore().collection(collection)
         return ref.doc(id).update(obj)
-
 }
