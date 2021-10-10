d61d96c0c55571fafae5b0c5f8d3e1166d6f75d3
MuazAlhaidar <omunam10@gmail.com>
2021-10-10 17:54:44 -0400

    Removing the android app

--- AndroidBeehive/Pages/Logout.js
+++
@@ -1,9 +0,0 @@
-import firebase from 'firebase';
-//logs the user out
-function Logout({navigation}) {
-  const auth = firebase.auth();
-  auth.signOut();
-  navigation.navigate('Login');
-  return null;
-}
-export default Logout;
--- AndroidBeehive/android/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  apiKey: 'AIzaSyBzNPS44siWEPbps4VelmGvi-SPkd6MNCk',
-
-  projectId: 'beehive-6cba8',
-};
-export default config;
